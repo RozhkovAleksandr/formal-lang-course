c3d14f178f1f482cf0d649cce98de953057cba56
Aleksandr <rozhkov_a@outlook.com>
2024-09-09 17:23:02 +0300

    add pytest test

--- project/task1.py
+++ project/task1.py
@@ -2,22 +2,26 @@ from dataclasses import dataclass
 import cfpq_data
 import networkx
 
-@dataclass
-class Graph:
-    edges_cnt: int
-    nodes_cnt: int
-    labels: list
 
-def graph_info(name):
+@dataclass
+class GraphInfo:
+    nodes: int
+    edges: int
+    labels: list[str]
+
+
+def get_graph_info(name):
     path = cfpq_data.download(name)
     graph = cfpq_data.graph_from_csv(path)
-    return Graph(
+    return GraphInfo(
         graph.number_of_nodes(),
         graph.number_of_edges(),
         cfpq_data.get_sorted_labels(graph),
     )
 
-def make_and_save_a_graph_of_two_cycles(n, m, labels, path):
+
+def save_to_pydot_labeled_two_cycles_graph(n, m, labels, path):
     graph = cfpq_data.labeled_two_cycles_graph(n, m, labels=labels)
-    pydot_graph = networkx.drawing.nx_pydot.to_pydot(graph)
-    pydot_graph.write_raw(path)
+    networkx.drawing.nx_pydot.write_dot(graph, path)
+
+save_to_pydot_labeled_two_cycles_graph(3, 2, ["a", "b"], "a.dot")
--- tests/test_task1.py
+++ tests/test_task1.py
@@ -1,43 +1,49 @@
-from project.task1 import *
+import pytest
+from project.task1 import (
+    save_to_pydot_labeled_two_cycles_graph,
+    get_graph_info,
+    GraphInfo
+)
 import networkx
 
 
 def test_graph_info():
-    actual_graph = graph_info("travel")
-    expected = Graph(
-        131,
-        277,
+    actual_graph = get_graph_info("generations")
+    expected = GraphInfo(
+        129,
+        273,
         [
             "type",
-            "subClassOf",
             "first",
             "rest",
-            "disjointWith",
             "onProperty",
-            "someValuesFrom",
-            "domain",
-            "range",
-            "comment",
-            "equivalentClass",
             "intersectionOf",
-            "differentFrom",
+            "equivalentClass",
+            "someValuesFrom",
             "hasValue",
-            "oneOf",
-            "minCardinality",
+            "hasSex",
+            "hasChild",
+            "hasParent",
             "inverseOf",
-            "hasPart",
-            "hasAccommodation",
-            "unionOf",
-            "complementOf",
-            "versionInfo"
+            "sameAs",
+            "hasSibling",
+            "oneOf",
+            "range",
+            "versionInfo",
         ],
     )
     assert actual_graph == expected
 
 
-def test_two_cycles_graph():
-    make_and_save_a_graph_of_two_cycles(3, 2, ["a", "b"], "test_graph.dot")
-    graph = networkx.DiGraph(networkx.drawing.nx_pydot.read_dot("test_graph.dot"))
+def test_graph_info_fail():
+    with pytest.raises(Exception):
+        get_graph_info("None")
+
+
+def test_save_to_pydot_labeled_two_cycles_graph(tmp_path):
+    path = tmp_path / "graph.dot"
+    save_to_pydot_labeled_two_cycles_graph(3, 2, ["a", "b"], path)
+    graph = networkx.DiGraph(networkx.drawing.nx_pydot.read_dot(path))
     edges = [
         (1, 2, dict(label="a")),
         (2, 3, dict(label="a")),
